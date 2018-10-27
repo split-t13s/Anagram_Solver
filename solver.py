ccb53829ea278d17beca4605a9e7533e043b6b0d
MS <email@example.com>
2018-10-27 19:09:41 +0100

    get_unique_letters added
    Returns unique letters from a given list

--- solver.py
+++ solver.py
@@ -6,6 +6,8 @@ from nltk.corpus import wordnet as wn
 all_words = wn.all_lemma_names()
 regexp = re.compile('^[A-Za-z]+$')      # Strings containing letters only
 
+test_letters = ["b", "e", "t", "t", "e", "r"]
+
 """
 Takes the list of all words and removes any word that does not match the
 specified regular expression 'regexp'.
@@ -18,4 +20,12 @@ def filter_all_words():
             temp = word + "\n"
             file.write(temp)
     file.close()
+
+def get_unique_letters(letters):
+    unique_letters = []
+    for letter in letters:
+        if letter not in unique_letters:
+            unique_letters.append(letter)
+    return unique_letters
+
 
