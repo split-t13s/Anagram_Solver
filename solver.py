566cd8fdcba36405eda3de5cc399629d3025e1c2
MS <email@example.com>
2018-10-27 18:55:50 +0100

    filter_all_words function added
    Checks all lemmas in wordnet and removes invalid words.
    Valid words stored in possiblewords.dat

--- solver.py
+++ solver.py
@@ -1,9 +1,21 @@
 import nltk
+import re
 
 from nltk.corpus import wordnet as wn
 
-letters = ["b", "e", "t", "t", "e", "r"]
+all_words = wn.all_lemma_names()
+regexp = re.compile('^[A-Za-z]+$')      # Strings containing letters only
 
-all_synsets = wn.words()
-
+"""
+Takes the list of all words and removes any word that does not match the
+specified regular expression 'regexp'.
+"""
+def filter_all_words():    
+    file = open("possiblewords.dat", "w")
+    for word in all_words:
+        result = regexp.search(word)
+        if result != None:
+            temp = word + "\n"
+            file.write(temp)
+    file.close()
 
