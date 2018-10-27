d3e9d3ead9d9391e11ebc1a1197a0dd55023a48d
MS <email@example.com>
2018-10-27 20:58:19 +0100

    get_initial_words modified
    Words containing only letters from list of unique letters now returned.

--- solver.py
+++ solver.py
@@ -32,8 +32,8 @@ def get_unique_letters(letters):
     return unique_letters
 
 """
-Returns words from all_words.dat that start with any unique letter from the list
-of given letters.
+Returns words from all_words.dat that only contain letters from the list of
+unique letters.
 """
 def get_initial_words(unique_letters):
     file = open("all_words.dat", "r")
@@ -42,10 +42,17 @@ def get_initial_words(unique_letters):
     initial_words = []
     for line in file_as_lines:
         word = line[:-1]
-        for letter in unique_letters:
-            if word.startswith(letter):
-                initial_words.append(word)
-                break
+        word_as_list = list(word)
+        matched = 0
+        for letter in word_as_list:
+            for unique_letter in unique_letters:
+                if letter == unique_letter:
+                    matched += 1
+        if matched == len(word_as_list):
+            initial_words.append(word)
     return initial_words
-    
+
+unique_letters = get_unique_letters(test_letters)
+initial_words = get_initial_words(unique_letters)
+print(initial_words)
 
