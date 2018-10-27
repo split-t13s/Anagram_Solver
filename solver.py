48230f7ca9052eec551d8b51b3b28a6c3d494340
MS <email@example.com>
2018-10-27 19:50:18 +0100

    get_initial_words added
    Words beginning with letters in the list passed to the method are extracted from all_words.dat

--- solver.py
+++ solver.py
@@ -21,9 +21,31 @@ def filter_all_words():
             file.write(temp)
     file.close()
 
+"""
+Returns all unique letters from a given list of letters.
+"""
 def get_unique_letters(letters):
     unique_letters = []
     for letter in letters:
         if letter not in unique_letters:
             unique_letters.append(letter)
     return unique_letters
+
+"""
+Returns words from all_words.dat that start with any unique letter from the list
+of given letters.
+"""
+def get_initial_words(letters):
+    unique_letters = get_unique_letters(letters)
+    file = open("all_words.dat", "r")
+    file_as_lines = file.readlines()
+    file.close()
+    initial_words = []
+    for line in file_as_lines:
+        for letter in unique_letters:
+            if line.startswith(letter):
+                initial_words.append(line)
+                break
+    return initial_words
+
+
