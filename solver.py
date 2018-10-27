ba2e699c9183d2ce744950d770bee1db05940978
MS <email@example.com>
2018-10-27 19:13:35 +0100

    possbilewords.dat renamed all_words.dat

--- solver.py
+++ solver.py
@@ -13,7 +13,7 @@ Takes the list of all words and removes any word that does not match the
 specified regular expression 'regexp'.
 """
 def filter_all_words():    
-    file = open("possiblewords.dat", "w")
+    file = open("all_words.dat", "w")
     for word in all_words:
         result = regexp.search(word)
         if result != None:
@@ -27,5 +27,3 @@ def get_unique_letters(letters):
         if letter not in unique_letters:
             unique_letters.append(letter)
     return unique_letters
-
-
