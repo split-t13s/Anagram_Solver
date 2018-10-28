bf0deda498470b4eeca4a35689ecde064dc11d87
MS <email@example.com>
2018-10-28 13:14:24 +0000

    get_final_words added
    Returns list of words that can be made using only the provided letters

--- solver.py
+++ solver.py
@@ -4,7 +4,7 @@ import re
 from nltk.corpus import wordnet as wn
 
 all_words = wn.all_lemma_names()
-regexp = re.compile('^[A-Za-z]+$')      # Strings containing letters only
+regexp = re.compile('^[A-Za-z]{3,}$')      # Strings with 3 or more letters
 
 test_letters = ["b", "e", "t", "t", "e", "r"]
 
@@ -52,7 +52,41 @@ def get_initial_words(unique_letters):
             initial_words.append(word)
     return initial_words
 
+def get_relevant_words(initial_words, all_letters):
+    dict_letters = letters_as_dict(all_letters)
+    final_words = []
+    for word in initial_words:
+        word_as_list = list(word)
+        for d_letter in dict_letters:
+            count = 0
+            for letter in word_as_list:
+                if d_letter == letter:
+                    count += 1
+                    if count > dict_letters[d_letter]:
+                        break
+                if letter == word_as_list[-1] \
+                   and d_letter == list(dict_letters.keys())[-1]:
+                    if word not in final_words:
+                        final_words.append(word)
+            else:
+                continue
+            break
+    return final_words
+
+"""
+Takes a list of letters and returns a dictionary of the unique letters, where
+the value is the number of times the same letter appears in the list.
+"""
+def letters_as_dict(letters):
+    dict_letters = {}
+    for letter in letters:
+        if letter in dict_letters:
+            dict_letters[letter] += 1
+        else:
+            dict_letters[letter] = 1
+    return dict_letters
+                   
+
 unique_letters = get_unique_letters(test_letters)
 initial_words = get_initial_words(unique_letters)
-print(initial_words)
-
+print(get_relevant_words(initial_words, test_letters))
